fd2dc726e72d0f45911caab97ac6f774b21dde4a
Austin S. Hemmelgarn <ahferroin7@gmail.com>
2019-08-12 15:06:22 -0400

    Add basic functionality to add and remove colors.

--- src/app.js
+++ src/app.js
@@ -1,9 +1,27 @@
 import {ColorCard} from './app/color-card.js'
 import {settings} from './app/settings.js'
 
+const elementCache = {
+    colorCardTemplate: document.querySelector('#colorCard'),
+    colors: document.querySelector('#colors'),
+}
+const cards = []
+
 settings.init()
 
-const baseColors = new ColorCard(document.querySelector('#baseColors'))
+cards.push(new ColorCard(document.querySelector('#baseColors')))
 
-baseColors.bg = '#ffffff'
-baseColors.text = '#000000'
+document.querySelector('#addColor').addEventListener('click', () => {
+    elementCache.colors.appendChild(elementCache.colorCardTemplate.content.cloneNode(true))
+
+    const target = elementCache.colors.lastElementChild
+    const wrapper = new ColorCard(target)
+
+    cards.push(wrapper)
+
+    target.querySelector('.close').addEventListener('click', () => {
+        const index = cards.indexOf(wrapper)
+        if (index !== -1) cards.splice(index, 1)
+        target.remove()
+    })
+})
